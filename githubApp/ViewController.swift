f0a5f909654737173f82ea255e24fdce4fd72475
佐々木翔太 <sasakishouta@sasakishoutanoiMac.local>
2021-11-27 00:23:28 +0900

    first commit

---
+++ githubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubApp
+//
+//  Created by 佐々木翔太 on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
